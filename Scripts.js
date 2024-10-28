571dd5f784dfc52ee59080c64d479ea207e4ee73
Maaybej <maay.bejosano@yahoo.com>
2024-10-27 21:03:09 -0400

    Add files via upload

---
+++ Scripts.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const title = document.querySelector('h1');
+    title.style.opacity = 0;
+
+    let fadeEffect = setInterval(() => {
+        if (!title.style.opacity) {
+            title.style.opacity = 1;
+        }
+        if (title.style.opacity < 1) {
+            title.style.opacity = parseFloat(title.style.opacity) + 0.01;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 50);
+});
